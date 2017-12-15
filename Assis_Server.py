36bbc3c0f086c8e52a37f38f32c998bcd2ee4a7a
Varnit Goel <17356399+varnitgoel@users.noreply.github.com>
2017-12-15 20:21:47 +0000

    Add files via upload

--- Assis_Server.py
+++ Assis_Server.py
@@ -13,8 +13,7 @@ import shelve
 class Assis_Server:
  
 # =============================================================================
-# Name_Assis is responsible of the mapping b/w directory direc 
-# and file servers.
+# Name_Assis is responsible of the mapping b/w directory names and file servers.
 # =============================================================================
 
     def GET(self, f_path):
@@ -23,28 +22,24 @@ class Assis_Server:
         if f_path == '/':
             return '\n'.join('%s=%s' % (d_path, _direc[d_path])
                     for d_path in sorted(_direc))
-
         d_path = str(os.path.dirname(f_path))
-
         if d_path in _direc:
             return _direc[d_path]
-
         raise web.not_found('No file server in this file.')
 #*****************************************************************************
     
-    def POST(self, d_path):
-         return _upgrade(str(d_path))
-     
+    def DELETE(self, d_path):
+        return _upgrade(str(d_path), False)    
+
 #*****************************************************************************
          
-    def DELETE(self, d_path):
-        return _upgrade(str(d_path), False)     
-
+    def POST(self, d_path):
+        return _upgrade(str(d_path))
+     
 #*****************************************************************************
     def _upgrade(d_path, add=True):  #Add pair of names to the Assis_Server
          web.header('Content-Type', 'text/plain; charset=UTF-8')
     i = web.input()
-
     if 'srv' not in i:
         raise web.bad_req() 
     srv = i['srv']
@@ -66,10 +61,27 @@ class Assis_Server:
             
     return 'OK' 
 
+#*****************************************************************************
+    
+def _upgrade_direc(d_path,srv,add=True):
+    if d_path[-1] == '/':
+        d_path = os.path.d_name(d_path)
+    if add:
+        logging.info('Update directory %s on %s.', d_path, srv)
+        _direc[d_path] = srv
+
+    elif d_path in _direc:
+        logging.info('Del. Direc. %s on %s.', d_path, srv)
+        del _direc[d_path]
+    else:
+        raise ValueError('%s Error!!' % d_path) 
+   
+#*****************************************************************************
     
 _config = {'dbfile': 'direc.db',
           }
 
 
-logging.info('Loading config file nameserver.dfs.json.')
+logging.info('Loading file Assis_Server.dfs.json.')
 _direc = shelve.open(_config['dbfile'])
+utils.load_config(_config, 'Assis_Server.dfs.json')
