6b03296c904fac6f8cdf58022faff852496e506f
Varnit Goel <17356399+varnitgoel@users.noreply.github.com>
2017-12-15 20:30:24 +0000

    Add files via upload

---
+++ file.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 14 19:40:28 2017
+
+@author: Varnit Goel
+"""
+
+#Server to hold and share the files
+
+import web
+import time
+import os.path
+
+#*****************************************************************************
+
+class FileServer:
+         def GET(self, f_path):
+              p = _get_local_path(f_path)
+              web.header('Last-Modified', time.ctime(os.path.getmtime(p)))
+              with open(p) as file:
+                  return file.read()
+              
+#*****************************************************************************           
+             
+         def PUT(self, f_path):
+            p = _get_local_path(f_path)
+            with open(p, 'w') as file:
+                file.write(web.data())
+            web.header('Last-Modified', time.ctime(os.path.getmtime(p)))
+            return()
+            
+#*****************************************************************************
