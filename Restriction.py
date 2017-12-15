d5ad0d497e0de2bee1c5f0418cc0865b15965131
Varnit Goel <17356399+varnitgoel@users.noreply.github.com>
2017-12-15 23:13:23 +0000

    Idea of to impose restrictions in a file
    Who can access at a point of time and how to delete previous restrictions etc.
    
    INCOMPLETE

--- Restriction.py
+++ Restriction.py
@@ -16,26 +16,33 @@ class Restrict:
           f_path = str(f_path)
           x = web.input()
           if f_path == '/':
-               
-               
-               
-               
-               
+                   str(_locks[f_path].granted),
+                   str(_locks[f_path].last_used),)
+                   for f_path in sorted(_locks))
+        elif f_path not in _locks and 'lock_id' not in i:
+            return 'OK'
+        elif 'lock_id' in i:
+            lock = _locks.get(f_path, -1)
+            try:
+                if int(i['lock_id']) == lock.lock_id:
+                    _update_lock(f_path)
+                    return 'OK'
+           else:
+               raise Exception("!Invalid!")
+ 
+#*****************************************************************************
+
+    def POST(self, f_path):
+
+        
+        
 # =============================================================================
-#                
-#           else:
-#               raise Exception("!Invalid!")
-#               
-#               
+# We could add few more definitions to make it better and useful like
+# new lock
+# delete lock
+# retract the previous lock etc
+#             
 # =============================================================================
-              
-              
-              
-    def POST(self, filepath):
-        f_path = str(f_path)
-        if f_path == '/':
-            allow_lock = {}
-            
             
             
             
